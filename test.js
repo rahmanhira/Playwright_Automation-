c447dc1e88c1ef37b9a0c1b938932d57fa081497
Mustafizur Rahman <rahmanhira@hotmail.com>
2023-02-04 01:24:15 -0600

    my 2nd js code

---
+++ test.js
@@ -0,0 +1,30 @@
+//this is a single line comment 
+/* this is 
+a multiline 
+comment (crtl + / )
+*/
+/*** This is a sample comment ***/
+
+console.log ('Hello World');
+
+/** Variables  - used to store data ,
+ *  can be looked as containers for storing data values
+ * Must not be a reserved keyword like if , else , boolean , let etc
+ * Variables are case sensitive
+ * Variables should have meaningful names - firstName , lastName
+ * Use camel casing - firstName, lastName 
+ *  */
+
+let x = 10;
+let a , b ;
+a = 21 ;
+b = 67 ;
+var u = 122;
+var o = 1314;
+var p, g ;
+p = 123;
+g = 12313;
+
+
+
+
